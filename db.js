d5116e7192367466b6ee8f93a3dcc463be65bcc9
akram3598786 <akramali03598786@gmail.com>
2022-09-14 01:02:21 +0530

    updated

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const redbus = require("./data/redbus.json");
 
 module.exports = {
-  items,
   tags,
   brands,
-  itemTypes
+  redbus
 }
 
 
